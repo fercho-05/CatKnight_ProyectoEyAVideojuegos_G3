cdbdbd6e8b1ac052ce80a0724e98c952f6287ea5
Xvale01 <valeriagc012004@gmail.com>
2023-11-26 01:34:47 -0600

    Movimiento top-down con animaciones del personaje

--- Assets/Scripts/CharacterController.cs
+++ Assets/Scripts/CharacterController.cs
@@ -4,15 +4,27 @@ using UnityEngine;
 
 public class CharacterController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    float moveSpeed = 5f;
 
-    // Update is called once per frame
+    public Rigidbody2D rb;
+    public Animator animator;
+
+    Vector2 movement;
+
     void Update()
     {
-        
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.y = Input.GetAxisRaw("Vertical");
+
+        animator.SetFloat("Horizontal", movement.x);
+        animator.SetFloat("Vertical", movement.y);
+        animator.SetFloat("Speed", movement.sqrMagnitude);
+    }
+
+
+    void FixedUpdate()
+    {
+        rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
     }
 }
+
